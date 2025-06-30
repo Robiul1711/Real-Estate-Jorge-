e822d5129ae1896fe27cf5577e109599a1e1d15c
najmul890890890 <najmul890890890@gmail.com>
2025-06-30 19:04:21 +0600

    updated in project view description component

--- src/components/admin/ProjectViewdescription.jsx
+++ src/components/admin/ProjectViewdescription.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import house from '@/assets/images/house.png'
+// import house from '@/assets/images/house.png'
+import house from '@/assets/images/home.png'
 const ProjectViewdescription = () => {
   return (
     <div>
